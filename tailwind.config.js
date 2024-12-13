80daa614b9408ae55c53a43a8b5f7a2ffd0b81f4
qty0 <peterszabototh@gmail.com>
2024-12-13 18:45:11 +0100

    huh

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -5,7 +7,7 @@ export default {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
   daisyui: {
     themes: ["light", "dark"],
