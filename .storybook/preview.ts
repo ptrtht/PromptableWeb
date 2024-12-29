35b8ade92560955fbde072b198d9b7a170ce2594
qty0 <peterszabototh@gmail.com>
2024-12-29 18:17:00 +0100

    x

---
+++ .storybook/preview.ts
@@ -0,0 +1,26 @@
+import type { Preview } from '@storybook/svelte';
+import '../src/app.css';
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
+
+const preview: Preview = {
+  decorators: [
+    withThemeByDataAttribute({
+      themes: {
+        light: 'light',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+      attributeName: 'data-theme',
+    }),
+  ],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
